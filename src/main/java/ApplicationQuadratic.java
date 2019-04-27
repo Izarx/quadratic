53d42205497e10ba2c84d81d47fa73cc099428c6
Ihor Zakharko <igor.zaharko@gmail.com>
2019-04-27 22:31:53 +0300

    create main method

--- src/main/java/ApplicationQuadratic.java
+++ src/main/java/ApplicationQuadratic.java
@@ -1,7 +1,25 @@
-public class ApplicationQuadratic
-{
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+
+@SpringBootApplication
+public class ApplicationQuadratic extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApplicationQuadratic.class);
+    }
+
+    @Bean
+    public Java8TimeDialect java8TimeDialect() {
+        return new Java8TimeDialect();
+    }
+
     public static void main(String[] args)
     {
-
+        SpringApplication.run(ApplicationQuadratic.class, args);
     }
 }
